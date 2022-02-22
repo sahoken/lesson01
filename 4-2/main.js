b25865aacd8bbc07f107a39a26c9df7422caf56b
sahoken <sahoken130@gmail.com>
2022-02-23 01:54:07 +0900

    4-2

---
+++ 4-2/main.js
@@ -0,0 +1,15 @@
+for(i=1; i<51; i++){
+    if(i%3===0){
+        console.log("WOW");
+    }else if(i-10===3){
+        console.log("WOW");
+    }else if(i-20===3){
+        console.log("WOW");
+    }else if(Math.floor(i/10)===3){
+        console.log("WOW");
+    }else if(i-40===3){
+        console.log("WOW");
+    }else{
+        console.log(i);
+    }
+};
